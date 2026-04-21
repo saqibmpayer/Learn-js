b79594c8f2b9c63f496dd820bba652c40b28493f
saqibmpayer <saqibmanzoorcodes@gmail.com>
2026-04-21 21:12:28 +0000

    variables lect

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144533;
+
+let accountEmail = "saqib@gmail.com"
+
+var accountPassword  = "123456"
+
+accountCity = 'kmr'
+
+// accountId = 122;  cannot be changed
+
+console.log(accountId);
+accountEmail = 's@s.com'
+accountPassword= "12341234"
+accountCity = "blr"
+
+console.table([accountId , accountEmail ,accountPassword , accountCity])
+
+
+
+
+
