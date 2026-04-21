1f506f11149df15efc35e6bbc32183d7e18f55e8
saqibmpayer <saqibmanzoorcodes@gmail.com>
2026-04-21 21:41:33 +0000

    conversionof DataTypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,18 @@
+let score = undefined
+// "33abc" Nan
+//  null  0
+// undefined => Nan
+// boolean => true = 1 . 
+
+
+
+ console.log(typeof(score));
+ console.log(typeof score);
+ 
+
+ let valueInNumber = Number(score);
+ console.log(valueInNumber); // NaN 
+  console.log(typeof valueInNumber);
+
+  // 1 => true ; 0 => false ;
+  // "" => false ; "rat" => true ;
