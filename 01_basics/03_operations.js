f4e7eeb1b1de44a172700a99116efeff067bffeb
saqibmpayer <saqibmanzoorcodes@gmail.com>
2026-04-26 19:10:53 +0000

    learned operations

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,26 @@
+let value = 3;
+
+let negValue = -value;
+
+ // console.log(negValue);
+
+//  let str1 = 'hello '
+
+//  let str2 = "saqib"
+
+//  let str3 = str1 + str2;
+ 
+//  console.log(str3);
+
+// console.log( 3 + 2 + "1");  51
+
+// console.log(+true); // 1
+
+// console.log(+""); // 0
+
+ let num1 , num2 , num3 = 4;
+
+
+
+// console.log(num3++); 4
+
