dc21ae7cfa99a4c10b27742d5e675e12311de051
saqibmpayer <saqibmanzoorcodes@gmail.com>
2026-04-26 21:44:36 +0000

    maths and num

---
+++ 01_basics/nums_and_maths.js
@@ -0,0 +1,36 @@
+const score = 400; 
+console.log(score);
+const balance = new Number(100);
+
+// console.log(balance); // [Number: 100]
+// console.log(balance.toString()); 
+// console.log(balance.toFixed(1));  // 
+
+const otherNumber = 123.8966;
+
+// console.log(otherNumber.toPrecision(3));
+
+const Hundreds = 100000;
+
+// console.log(Hundreds.toLocaleString("en-IN"));
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4 , 3 , 5  ,  6 ));
+// console.log(Math.max(4 , 3 , 5  ,  6 ));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+
+const min = 10 
+const max = 20 
+
+console .log(Math.floor(Math.random() * (max - min + 1 ))+ min);
