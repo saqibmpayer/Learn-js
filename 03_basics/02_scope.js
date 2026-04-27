4a9a132a5e1f266a1880dc1edfe5815c09700328
saqibmpayer <saqibmanzoorcodes@gmail.com>
2026-04-27 18:50:54 +0000

    HOISTING

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,15 +1,45 @@
 
 
-var c = 300;
-let a = 100;
-if (true) {
-let a = 10 ;
-const b = 30 ;
-c = 40;
-console.log("INNER: " , a); // 100
+// var c = 300;
+// let a = 100;
+// if (true) {
+// let a = 10 ;
+// const b = 30 ;
+// c = 40;
+// console.log("INNER: " , a); // 100
+// }
+
+
+
+// console.log(a); // 100
+
+function one(){
+    const username = "Saqib"
+
+    function two(){
+         const website = "youtube"
+    
+         console.log(username);
+         
+    }
+
+    //  console.log(website);
+     two()
+
 }
 
+// one();
 
+// INTRESTING ++++++++++++++++++++++
 
-console.log(a); // 100
+console.log(addOne(5))
+function addOne(num){
+    return num+1;
 
+}
+
+//HOISTING
+//  addTwo(5)  // ERROR
+// const addTwo = function(num){
+//     return num + 2;
+// }
