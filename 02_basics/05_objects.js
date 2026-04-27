d85343e8ed2b8aba3d40b3505ae536fabad9502d
saqibmpayer <saqibmanzoorcodes@gmail.com>
2026-04-27 11:04:27 +0000

    json

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,23 @@
+const course = {
+    coursename: "js in Hindi",
+    price:"999",
+    courseInstrutor: 'hitesh'
+}
+
+//cousrse.courseInstrutor
+
+const{courseInstrutor :Instrutor} =  course
+
+console.log(Instrutor);
+
+// JSON
+{
+'name' : "hitesh",
+'coursename': "js in Hindi",
+'price':"free"
+}
+
+[
+    {},
+    {},
+]
