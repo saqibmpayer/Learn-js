9bd9eed6b7a23f40bcdbeeb2d99967996f2f1ae2
saqibmpayer <saqibmanzoorcodes@gmail.com>
2026-04-27 10:43:46 +0000

    still on objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -7,6 +7,54 @@ tinderUser.name = 'sammy';
 
 tinderUser.isLoggedIn = false;
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
 
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullName:{
+        userfullName : {
+            firstName:"saqib",
+            lastName: "choudary"
+        }
+    }
+}
+// console.log(regularUser?.fullName.userfullName.firstName);
+
+const obj1 = {
+    1 : "a",
+    2:'b',
+}
+const obj2 = {
+    3 : "c",
+    4:'d',
+}
+
+// const obj3 = {obj1, obj2}
+
+// obj3 = Object.assign({} ,obj1 , obj2)
+// console.log(obj3);
+
+// const obj3 = {...obj1 , ...obj2}
+
+// console.log(obj3);
+
+const users=[
+    {
+        id: 1,
+        email: "h@user.com"
+    },{
+
+    },{
+
+    }
+]
+// users[1].email
+
+// console.log(Object.keys(tinderUser));
+
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
