24381e4eec908fac3962502cf4b4f1eb0e658036
saqibmpayer <saqibmanzoorcodes@gmail.com>
2026-04-27 10:09:25 +0000

    mastering objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,12 @@
+// const tinderUser = new Object();
+const tinderUser= {}
+
+
+tinderUser.id = "12abc";
+tinderUser.name = 'sammy';
+
+tinderUser.isLoggedIn = false;
+
+console.log(tinderUser);
+
+
