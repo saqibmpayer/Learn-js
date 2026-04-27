4caaeea575d845d66d792ba36ce4d7bb29c613ac
saqibmpayer <saqibmanzoorcodes@gmail.com>
2026-04-27 20:40:55 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,11 @@
+//IMMEDIATELY INVOKED FUNCTION EXPRESSION(iiFE)
+
+
+// (function  chai(){
+// Named iffe
+//     console.log("DB CONNECTED!");
+    
+// })()
+
+((name) => (console.log(`db Connected Two ${name}`)
+))("saqib");
