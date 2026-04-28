59c0fbdac88daf722b5f0166c453fa6e22fb44a0
saqibmpayer <saqibmanzoorcodes@gmail.com>
2026-04-28 09:45:14 +0000

    mastering conditionals

---
+++ 04_controlflow/one.js
@@ -0,0 +1,49 @@
+
+
+// if
+const isUserLoggedIn = true;
+
+if (isUserLoggedIn) {
+    
+}
+// if (false) {
+
+// }
+//  const balance = 11000;
+// // if(balance > 1000)console.log("test"),(console.log("test2"));
+
+// if (balance < 500){
+//     console.log("Less than 500");
+    
+// }
+
+// else if (balance < 750){
+//     console.log("Less than 750");
+    
+// }
+// else if (balance < 900){
+//     console.log("Less than 900");
+    
+// }
+// else{
+//     console.log("greater than 900");
+    
+// }
+
+
+// const UserLoggedIn = true;
+// const debitCard = true ;
+// const logginFromGoogle = false;
+// const logginFromEmail = false;
+
+
+// if (UserLoggedIn && debitCard ){
+//     console.log("Allow to Buy");
+    
+// }
+
+// if(logginFromEmail || logginFromGoogle) {
+//     console.log("USER LOGGED IN");
+    
+// }
+
---
+++ 04_controlflow/truthy.js
@@ -0,0 +1,51 @@
+// // const UserEmail = "user@ai"
+
+// // const UserEmail = "" // Don't Have user Email
+// const UserEmail = []
+
+// if(UserEmail){
+//     console.log("Got User Email");
+    
+// }
+// else{
+//     console.log("Don't Have user Email");
+    
+// }
+
+// false Values => false , 0 , -0 , "" , BigInt 0n ,null ,Nan ,undefined
+
+//truthy value => "0" , " ",[] ,{} ,function(){}
+
+
+// if(UserEmail.length === 0){
+//     console.log("Array is empty");
+    
+// }
+
+// const Obj = {}
+
+// if (Object.keys(Obj).length === 0){
+//     console.log("Object is empty ");
+    
+// }
+
+// NULLISH Coalescing Operator (??):null undefined
+
+let val1;
+
+// val1 = 5 ?? 10;
+
+// val1 = null ?? 10;
+ 
+// val1 = undefined ?? 15;
+
+// val1 = null ?? 20 ?? 30;
+
+
+
+// console.log(val1);
+
+
+/// ternery Operator 
+
+// condition? true:false
